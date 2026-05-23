6292fccbb39069c9ebf6d84596f52e044ed2ecab
SVarun22 <vsekhar542@gmail.com>
2026-05-23 18:00:40 +0530

    changes done

--- Ecommerce_App/Frontend/src/App.jsx
+++ Ecommerce_App/Frontend/src/App.jsx
@@ -14,6 +14,7 @@ import PlaceOrder from './pages/PlaceOrder'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
+import Verify from './pages/Verify'
 
 function App() {
   return (
@@ -31,6 +32,8 @@ function App() {
       <Route path='/login' element={<Login/>}/>
       <Route path='/place-order' element={<PlaceOrder/>}/>
       <Route path='/orders' element={<Orders/>}/>
+      <Route path='/verify' element={<Verify/>}/>
+      
     </Routes>
     <Footer/>
    </div>
--- Ecommerce_App/Frontend/src/components/CartTotal.jsx
+++ Ecommerce_App/Frontend/src/components/CartTotal.jsx
@@ -13,7 +13,7 @@ const CartTotal = () => {
         <Title text1={'CART'} text2={'TOTALS'}/>
       </div>
 
-      <div className='flex flex-col gap-2 mt-2 text-sm'v>
+      <div className='flex flex-col gap-2 mt-2 text-sm'>
         <div className='flex justify-between'>
             <p>Subtotal</p>
             <p>{currency} {getCartAmount()}.00</p>
