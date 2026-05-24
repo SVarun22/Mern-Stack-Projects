12790a2eca5e68397154a647a352baa1db0106f6
SVarun22 <vsekhar542@gmail.com>
2026-05-25 01:50:22 +0530

    Quick AI web app-add routes and create Home navbar and Hero section

---
+++ QUICKAI/client/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import WriteArticle from './pages/WriteArticle'
+import BlogTitles from './pages/BlogTitles'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import ReviewResume from './pages/ReviewResume'
+import RemoveObject from './pages/RemoveObject'
+import Community from './pages/Community'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/ai' element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path='write-article' element={<WriteArticle/>}/>
+          <Route path='blog-titles' element={<BlogTitles/>}/>
+          <Route path='generate-images' element={<GenerateImages/>}></Route>
+          <Route path='remove-background' element={<RemoveBackground/>}></Route>
+          <Route path='remove-object' element={<RemoveObject/>}></Route>
+          <Route path='resume-review' element={<ReviewResume/>}></Route>
+          <Route path='community' element={<Community/>}></Route>
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
